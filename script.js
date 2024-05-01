29571ed6fdce5e1d328fe53ab54cab83865733e6
ares <58524779+ar3s-xss@users.noreply.github.com>
2024-05-01 20:44:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function link1(){
+    document.getElementById("bGlua18xCg==").innerHTML = "PROJECTS&nbsp;&nbsp;&nbsp;";
+}
+
+function link11(){
+    document.getElementById("bGlua18xCg==").innerHTML = "bGlua18xCg==&nbsp;&nbsp;&nbsp;";
+}
+
+function link2(){
+    document.getElementById("bGlua3MK").innerHTML = "LINKS&nbsp;&nbsp;&nbsp;";
+}
+
+function link22(){
+    document.getElementById("bGlua3MK").innerHTML = "bGlua3MK&nbsp;&nbsp;&nbsp;";
+}
+
+function link3(){
+    document.getElementById("YWJvdXQK").innerHTML = "ABOUT&nbsp;&nbsp;&nbsp;";
+}
+
+function link33(){
+    document.getElementById("YWJvdXQK").innerHTML = "YWJvdXQK&nbsp;&nbsp;&nbsp;";
+}
+
+function link4(){
+    document.getElementById("QVJFU1hTUwo=").innerHTML = "ARESXSS";
+}
+
+function link44(){
+    document.getElementById("QVJFU1hTUwo=").innerHTML = "QVJFU1hTUwo=";
+}
+
